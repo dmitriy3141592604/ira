3891552ec368d93043f3473672b5b558efc70a7f
Dmitry Frolov <frolovdv@it2g.ru>
2016-06-07 19:16:37 +0300

    Изменено оглавление процессов

---
+++ ira/src/main/java/org/i2g/ira/complaint/save/p01CorrectCheckingDate.java
@@ -0,0 +1,14 @@
+package org.i2g.ira.complaint.save;
+
+import org.springframework.stereotype.Component;
+
+import utils.Complaint;
+import utils.ComplaintSaveProcessBase;
+
+@Component
+public class p01CorrectCheckingDate extends ComplaintSaveProcessBase {
+
+	public void execute(Complaint t) {
+	}
+
+}
---
+++ ira/src/main/java/org/i2g/ira/complaint/save/p05PersistComplaint.java
@@ -0,0 +1,16 @@
+package org.i2g.ira.complaint.save;
+
+import org.springframework.stereotype.Component;
+
+import utils.Complaint;
+import utils.ComplaintSaveProcessBase;
+
+@Component
+public class p05PersistComplaint extends ComplaintSaveProcessBase {
+
+	public void execute(Complaint t) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
