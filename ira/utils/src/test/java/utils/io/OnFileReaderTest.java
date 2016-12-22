f373d2ca2a5920fb4d4c3491d7d73052c47ad668
Denis Timofeev <timofeevdv@it2g.ru>
2016-12-22 23:21:34 +0300

    Add OnFileReader tests

--- ira/utils/src/test/java/utils/io/OnFileReaderTest.java
+++ ira/utils/src/test/java/utils/io/OnFileReaderTest.java
@@ -1,79 +1,76 @@
 package utils.io;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+//import static org.hamcrest.Matcher.*;
+import static org.hamcrest.CoreMatchers.isA;
+import static org.junit.Assert.*;
 import static utils.Value.newValue;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.junit.AfterClass;
 import org.junit.Test;
 
 import utils.Value;
 
 public class OnFileReaderTest extends OnFileReaderTestBase {
-	/*
-	 * @Test public void test$staticConstructor() { createExchangePoint();
-	 * OnFileWriter.onFileWriter(exchangePoint, pw -> { }); }
-	 */
-
-	@Test
-	public void testTest() {
-		assertTrue(true);
-	}
-
-	String line;
-
+	
 	@Test
 	public void testFileReading() throws Exception {
 
+		File tmpFile = super.newFile();
+		System.out.println(tmpFile.getAbsolutePath());
+		
+		try(FileWriter fileWriter = new FileWriter(tmpFile)) {
+			fileWriter.write(ethalon);
+		}
 		final Value<String> value = newValue();
 
+		OnFileReader fileReader = new OnFileReader(tmpFile);
+		fileReader.accept( (t) -> value.setValue(t.readLine()));
+		assertEquals(ethalon, value.getValue());
+
+	}
+	
+	@Test
+	public void testConstructorWithNullArgument() {
+		exception.expectCause(isA(NullPointerException.class));
+		new OnFileReader(null);
+	}
+	
+	@Test
+	public void testConstructorReturnValue() {
+		createExchangePoint();
+		OnFileReader fileReader = new OnFileReader(exchangePoint);
+		assertNotNull(fileReader);
+	}
+	
+	@Test
+	public void testExceptionHandling() {
+		exception.expect(RuntimeException.class);
+		
 		execute(new ExceptionConsumer<BufferedReader>() {
 
 			@Override
 			public void accept(BufferedReader t) throws Exception {
-				value.setValue(t.readLine());
+				throw new IOException("Some exception");
 			}
 		});
-
-		assertEquals("11111111", value.getValue());
-
-		//
-		// try (final BufferedReader reader = new BufferedReader(new
-		// FileReader(exchangePoint))) {
-		// value.setValue(reader.readLine());
-		// }
-		//
-		// assertEquals("", value.getValue());
-
 	}
-
-	/** Заметим: Нет декларации исключения **/
-	/*
-	 * @Test public void test$exceptionHandling() {
-	 *
-	 * exception.expect(RuntimeException.class);
-	 *
-	 * execute(out -> { throw new IOException("some error"); });
-	 *
-	 * execute(new ExceptionConsumer<BufferedReader>() {
-	 *
-	 * @Override public void accept(BufferedReader t) throws Exception { throw
-	 * new IOException("some error"); } }); }
-	 */
-
-	/*
-	 * @Test public void test$unexpectedFileNameException() {
-	 * exception.expect(RuntimeException.class); new OnFileWriter(new
-	 * File("/")); }
-	 *
-	 * @Test public void test$WriterConstructorTest() { final StringWriter
-	 * stringWriter = new StringWriter(); new
-	 * OnFileWriter(stringWriter).accept(out -> out.print("hello"));
-	 * assertEquals("hello", stringWriter.toString()); }
-	 *
-	 * @Test public void test$null() { exception.expect(RuntimeException.class);
-	 * new OnFileWriter((Writer) null).accept(out -> out.print("hello"));
-	 * //assertEquals("hello", stringWriter.toString()); }
-	 */
+	
+	@AfterClass
+	public static void testTmpFilesRemoval() {
+		Boolean exists = false;
+		for(String s : tmpFiles) {
+			Path path = Paths.get(s);
+			exists |= Files.exists(path);
+		}
+		assertFalse(exists);
+	}
+	
 }
