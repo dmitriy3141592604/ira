bf42dc1e7924d2632e74be555e44e864b31a1e54
Dmitry <User@WIN-T1J2J5B8A1T>
2016-12-22 23:09:15 +0200

    Сделана новая заготовка для TabbedPaneComponent

---
+++ ira/uiserializer/src/main/java/uiserializer/components/TabbedPaneComponent.java
@@ -0,0 +1,13 @@
+package uiserializer.components;
+
+import org.i2g.ira.uibuilder.Attributes;
+import org.i2g.ira.uibuilder.HTMLElements;
+
+public class TabbedPaneComponent implements Attributes, Component {
+
+	@Override
+	public void render(HTMLElements html) {
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ ira/uiserializer/src/test/java/uiserializer/components/TabbedPaneCompnentTest.java
@@ -0,0 +1,29 @@
+package uiserializer.components;
+
+import static org.junit.Assert.assertEquals;
+
+import org.i2g.ira.uibuilder.HTMLElements;
+import org.junit.Test;
+
+public class TabbedPaneCompnentTest extends ComponentTestBase<TabbedPaneComponent> {
+
+	@Override
+	protected TabbedPaneComponent newComponent() {
+		return new TabbedPaneComponent() {
+
+			@Override
+			public void render(HTMLElements html) {
+				final HTMLElements header = html.ul();
+				header.li(klass(".tab-header"));
+			}
+
+		};
+	}
+
+	@Test
+	public void test$001() {
+		assertEquals(1, occurenceCount("//self::node()[@class = '.tab-header']"));
+
+	}
+
+}
