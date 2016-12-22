50e8339d3c1199c61c757660c9bcafd7d01ef087
Dmitry <User@WIN-T1J2J5B8A1T>
2016-12-22 20:37:16 +0200

    TabbedCompoenentTest removed

--- ira/uiserializer/src/test/java/uiserializer/components/ComponentTestBase.java
+++ ira/uiserializer/src/test/java/uiserializer/components/ComponentTestBase.java
@@ -1,48 +1,5 @@
 package uiserializer.components;
 
-import java.io.File;
-import java.util.Set;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.rules.TestName;
-
-// FIXME Восстанови меня !
 public abstract class ComponentTestBase {
-
-	@Rule
-	public TestName name = new TestName();
-
-	// protected UIBuilderFactoryBuilder builder;
-
-	protected Set<String> attributeValues;
-
-	protected Set<String> tagNames;
-
-	@Before
-	public final void setUpComponentTestBase() {
-		// builder = new UIBuilderFactoryBuilder();
-		// builder.build();
-		// attributeValues = builder.getAttributeValues();
-		// tagNames = builder.getTagNames();
-	}
-
-	@After
-	public final void tearDown() {
-		final String simpleOutputFileName = this.getClass() + "$" + name.getMethodName();
-		final File outputFileName = new File("html/" + simpleOutputFileName + ".html");
-
-		// new OnFileWriter(outputFileName).accept((out) ->
-		// out.print(builder.getSb()));
-	}
-
-	// protected String serializeContentToInternalBuffer(boolean b) {
-	// final String retVal = builder.serializeContentToInternalBuffer();
-	// if (b) {
-	// System.out.println(retVal);
-	// }
-	// return retVal;
-	// }
 
 }
