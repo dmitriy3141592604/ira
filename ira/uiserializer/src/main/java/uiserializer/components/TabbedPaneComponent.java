e56df21ff3e5bb23adc4188f04ab8407a5cb474c
Dmitry <User@WIN-T1J2J5B8A1T>
2016-12-23 22:23:17 +0200

    TabbedComponent # Progress

--- ira/uiserializer/src/main/java/uiserializer/components/TabbedPaneComponent.java
+++ ira/uiserializer/src/main/java/uiserializer/components/TabbedPaneComponent.java
@@ -3,11 +3,16 @@ package uiserializer.components;
 import org.i2g.ira.uibuilder.Attributes;
 import org.i2g.ira.uibuilder.HTMLElements;
 
+import utils.Responsibility;
+
+@Responsibility("Создает UI элемент с заголовками, которые переключают содержимое основной панели")
 public class TabbedPaneComponent implements Attributes, Component {
 
 	@Override
 	public void render(HTMLElements html) {
-		throw new UnsupportedOperationException();
+		final HTMLElements header = html.ul();
+		header.li(klass("tab-headers"));
+		header.div(klass("tab-panes"));
 	}
 
 }
--- ira/uiserializer/src/test/java/uiserializer/components/TabbedPaneCompnentTest.java
+++ ira/uiserializer/src/test/java/uiserializer/components/TabbedPaneCompnentTest.java
@@ -2,27 +2,23 @@ package uiserializer.components;
 
 import static org.junit.Assert.assertEquals;
 
-import org.i2g.ira.uibuilder.HTMLElements;
 import org.junit.Test;
 
 public class TabbedPaneCompnentTest extends ComponentTestBase<TabbedPaneComponent> {
 
 	@Override
 	protected TabbedPaneComponent newComponent() {
-		return new TabbedPaneComponent() {
-
-			@Override
-			public void render(HTMLElements html) {
-				final HTMLElements header = html.ul();
-				header.li(klass(".tab-header"));
-			}
-		};
+		return new TabbedPaneComponent();
 	}
 
 	@Test
-	public void test$001() {
-		assertEquals(1, occurenceCount("//self::node()[@class = '.tab-header']"));
+	public void test$headerConstructed() {
+		assertEquals(1, occurenceCount("//self::node()[@class = 'tab-headers']"));
+	}
 
+	@Test
+	public void test$bodyConstructed() {
+		assertEquals(1, occurenceCount("//self::node()[@class = 'tab-panes']"));
 	}
 
 }
