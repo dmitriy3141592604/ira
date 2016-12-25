18898dc1c82619a7f8a627f81a13a022c1f13c5e
Dmitry <User@WIN-T1J2J5B8A1T>
2016-12-25 23:11:26 +0200

    Добавлены элементы. Отсортировано.

--- ira/uibuilder/src/main/java/org/i2g/ira/uibuilder/HTMLElements.java
+++ ira/uibuilder/src/main/java/org/i2g/ira/uibuilder/HTMLElements.java
@@ -1,38 +1,15 @@
 package org.i2g.ira.uibuilder;
 
 public interface HTMLElements {
-
-	HTMLElements form(Attribute... attrs);
-
-	HTMLElements input(Attribute... attributes);
-
-	HTMLElements label(Attribute... attrs);
-
-	HTMLElements text(String text);
-
-	HTMLElements head(Attribute... attrs);
-
-	HTMLElements title(Attribute... attrs);
-
-	HTMLElements meta(Attribute charset);
-
-	HTMLElements link(Attribute... attrs);
+	HTMLElements a(Attribute... attrs);
 
 	HTMLElements body(Attribute... attrs);
 
+	HTMLElements button(Attribute... attrs);
+
 	HTMLElements div(Attribute... attrs);
 
-	HTMLElements ul(Attribute... attrs);
-
-	HTMLElements li(Attribute... attrs);
-
-	HTMLElements script(Attribute... attrs);
-
-	HTMLElements tr(Attribute... attrs);
-
-	HTMLElements td(Attribute... attrs);
-
-	HTMLElements table(Attribute... attributes);
+	HTMLElements form(Attribute... attrs);
 
 	HTMLElements h1(Attribute... attributes);
 
@@ -49,5 +26,31 @@ public interface HTMLElements {
 	HTMLElements h7(Attribute... attributes);
 
 	HTMLElements h8(Attribute... attributes);
+
+	HTMLElements head(Attribute... attrs);
+
+	HTMLElements input(Attribute... attributes);
+
+	HTMLElements label(Attribute... attrs);
+
+	HTMLElements li(Attribute... attrs);
+
+	HTMLElements link(Attribute... attrs);
+
+	HTMLElements meta(Attribute charset);
+
+	HTMLElements script(Attribute... attrs);
+
+	HTMLElements table(Attribute... attributes);
+
+	HTMLElements td(Attribute... attrs);
+
+	HTMLElements text(String text);
+
+	HTMLElements title(Attribute... attrs);
+
+	HTMLElements tr(Attribute... attrs);
+
+	HTMLElements ul(Attribute... attrs);
 
 }
