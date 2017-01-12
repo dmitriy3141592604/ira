e0490a5752abebbd968bdb382f0d57ec7f848ebf
Dmitry <User@WIN-T1J2J5B8A1T>
2017-01-12 23:59:46 +0200

    Рефакторинк ApplicationSerializerBase

--- ira/uiserializer/src/main/java/uiserializer/ApplicationSerializerBase.java
+++ ira/uiserializer/src/main/java/uiserializer/ApplicationSerializerBase.java
@@ -7,15 +7,18 @@ import org.i2g.ira.uibuilder.HTMLElements;
 
 public abstract class ApplicationSerializerBase<T> implements WithSimpleClassName {
 
+	private static final Class<HTMLElements> INTERFACE_CLASS = HTMLElements.class;
+
 	private final ApplicationSerializerConfiguration configuration = new ApplicationSerializerConfiguration(this);
 
 	protected abstract void build(T app, ComponentBuilder cb);
 
 	public void process(PrintWriter out, Class<T> applicationClass) {
-		final StringBuilder stringBuilder = new StringBuilder();
-		final UIBuilderBuilder uiBuilderBuilder = new UIBuilderBuilder();
-		final Class<HTMLElements> interfaceClass = HTMLElements.class;
-		final Element element = new Element("html");
+		// TASK Рефакторинк ApplicationSerializerBase
+		final StringBuilder sb = newStringBuilder();
+		final UIBuilderBuilder uiBuilderBuilder = newUIBuilderBuilder();
+		final Class<HTMLElements> interfaceClass = INTERFACE_CLASS;
+		final Element element = newRootElement();
 		final UIBuilder uiBuilder = uiBuilderBuilder.setInterface(interfaceClass).setElement(element).build();
 		final T app = newApplication(applicationClass);
 		final ComponentBuilder cb = newBodyBuilder();
@@ -23,12 +26,23 @@ public abstract class ApplicationSerializerBase<T> implements WithSimpleClassNam
 		build(app, cb);
 
 		cb.render(uiBuilder.getHtml());
-		uiBuilder.serializeContent(stringBuilder);
+		uiBuilder.serializeContent(sb);
 
-		final String sc = stringBuilder.toString();
-		out.println(sc);
+		out.println(sb);
 
-		configuration.getWriter().accept(f -> f.println(sc));
+		configuration.getWriter().accept(f -> f.println(sb));
+	}
+
+	private Element newRootElement() {
+		return new Element("html");
+	}
+
+	private UIBuilderBuilder newUIBuilderBuilder() {
+		return new UIBuilderBuilder();
+	}
+
+	private StringBuilder newStringBuilder() {
+		return new StringBuilder();
 	}
 
 	protected Empty newEmpty() {
@@ -47,7 +61,8 @@ public abstract class ApplicationSerializerBase<T> implements WithSimpleClassNam
 		return new PageLayout();
 	}
 
-	protected BodyBuilder newBodyBuilder() {
+	// TODO Переместить в статический конструктор
+	private BodyBuilder newBodyBuilder() {
 		return new BodyBuilder();
 	}
 
