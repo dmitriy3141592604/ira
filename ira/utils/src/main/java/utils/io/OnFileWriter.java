4405ad1d8afc85434679162b9b5d20bfe2c9f7f3
Denis Timofeev <nakolenke@gmail.com>
2017-01-18 22:17:19 +0300

    WIP: Отложенная инициализация потока записи

--- ira/utils/src/main/java/utils/io/OnFileWriter.java
+++ ira/utils/src/main/java/utils/io/OnFileWriter.java
@@ -1,51 +1,50 @@
 package utils.io;
 
-import static utils.Safer.safe;
-
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.Writer;
-import java.nio.file.Path;
 
+import utils.ExceptionSupplier;
 import utils.Responsibility;
 
 /**
- * Предоставляет интерфейс для возможности записи в файл или Writer без необходимости объявлять функцию с throws IOException или ловить IOException.
+ * Предоставляет интерфейс для возможности записи в файл или Writer без
+ * необходимости объявлять функцию с throws IOException или ловить IOException.
  *
  */
 @Responsibility("Скрывает особенности жизненного цикла файлого потока на запись. В том числе исключительные ситуации")
 public class OnFileWriter {
 
-	public static void onFileWriter(File file, ExceptionConsumer<PrintWriter> f) {
-		new OnFileWriter(file).accept(f);
+	// public static void onFileWriter(File file, ExceptionConsumer<PrintWriter>
+	// f) {
+	// new OnFileWriter(file).accept(f);
+	// }
+
+	private ExceptionSupplier<Writer> writerSource;
+
+	public OnFileWriter(ExceptionSupplier<Writer> writerSource) {
+		if (writerSource == null) {
+			throw new IllegalArgumentException("Writer source cannot be null");
+		}
+		this.writerSource = writerSource;
 	}
 
-	private final Writer writer;
+	public OnFileWriter(File file) {
+		this(new ExceptionSupplier<Writer>() {
 
-	public OnFileWriter(Writer writer) {
-		this.writer = writer;
-	}
-
-	public OnFileWriter(File exchangePoint) {
-		this.writer = safe(() -> new FileWriter(exchangePoint));
-	}
-
-	// XXX Not tested
-	public OnFileWriter(Path path) {
-		this(path.toFile());
+			@Override
+			public Writer get() throws Exception {
+				return new FileWriter(file);
+			}
+		});
 	}
 
 	// beforeAccept
 	public final void accept(ExceptionConsumer<PrintWriter> f) {
-		try (final PrintWriter out = wrap()) {
+		try (final PrintWriter out = new PrintWriter(writerSource.safe())) {
 			f.safe(out);
 		}
-	}
-
-	private PrintWriter wrap() {
-		return new PrintWriter(new BufferedWriter(writer));
 	}
 
 }
