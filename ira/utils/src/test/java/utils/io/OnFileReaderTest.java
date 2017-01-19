03254a3cf53395081618ec37d3078582994b2a8c
Frolov_D <Frolov_D@FROLOV_D.bpc.in>
2017-01-19 19:21:34 +0300

    Test fixed

--- ira/utils/src/test/java/utils/io/OnFileReaderTest.java
+++ ira/utils/src/test/java/utils/io/OnFileReaderTest.java
@@ -25,9 +25,12 @@ public class OnFileReaderTest extends OnFileReaderTestBase {
 	public void testFileReading() throws Exception {
 
 		/**
-		 * TODO REVIEW(fdv): super избыточно. Если в текущем классе переопределить newFile() (Для логгирования или дебага, то пользователя ждет
-		 * сюрприз tdv: Переопределил - на мой взгляд ничего не изменилось, тест работает по-старому. Если же речь о том, что пользователь не сможет
-		 * вызвать метод newFile, определённный в текущем класса, то ему не нужно использовать "super"
+		 * TODO REVIEW(fdv): super избыточно. Если в текущем классе
+		 * переопределить newFile() (Для логгирования или дебага, то
+		 * пользователя ждет сюрприз tdv: Переопределил - на мой взгляд ничего
+		 * не изменилось, тест работает по-старому. Если же речь о том, что
+		 * пользователь не сможет вызвать метод newFile, определённный в текущем
+		 * класса, то ему не нужно использовать "super"
 		 **/
 		final File tmpFile = super.newFile();
 		newFile();
@@ -58,14 +61,15 @@ public class OnFileReaderTest extends OnFileReaderTestBase {
 	@Test
 	public void testConstructorWithNullArgument() {
 		exception.expect(NullPointerException.class);
-		new OnFileReader(null);
+		new OnFileReader((File) null);
 	}
 
 	@Test
 	public void testConstructorReturnValueIsNotNull() {
 		/**
-		 * TODO REVIEW(fdv): запутано. Если посмотреть комменты к createExchangePoint - то это место, где дизайн выходит боком. Объяснить сложно,
-		 * логика теста запутана. Нужно обсудить словами.
+		 * TODO REVIEW(fdv): запутано. Если посмотреть комменты к
+		 * createExchangePoint - то это место, где дизайн выходит боком.
+		 * Объяснить сложно, логика теста запутана. Нужно обсудить словами.
 		 **/
 		createExchangePoint();
 		final OnFileReader fileReader = new OnFileReader(exchangePoint);
@@ -76,12 +80,17 @@ public class OnFileReaderTest extends OnFileReaderTestBase {
 	public void testConstructorWithMissingFile() {
 		exception.expectCause(isA(FileNotFoundException.class));
 		/**
-		 * TODO REVIEW(fdv): В любой *nix системе есть корень с именем / При работе в linux исключение FileNotFound будет звучать очень странно.
-		 * Корневой каталог всегда существует. (По крайней мене, если запустилась java и maven tdv: Пока не придумал, как сделать иначе, поскольку
-		 * способ "получить имя несуществующего файла и использовать его" не является атомарной операцией. В результате файл может быть создан между
-		 * проверкой на существование и вызовом конструктора (следующая строка). В Linux насколько я знаю в именах файлов недопустим символ "/", но,
-		 * скажем, проверять наличие файла "x/y" не имеет смысла, поскольку "x/y" будет интерпретирован как путь, который теоретически может
-		 * существовать.
+		 * TODO REVIEW(fdv): В любой *nix системе есть корень с именем / При
+		 * работе в linux исключение FileNotFound будет звучать очень странно.
+		 * Корневой каталог всегда существует. (По крайней мене, если
+		 * запустилась java и maven tdv: Пока не придумал, как сделать иначе,
+		 * поскольку способ "получить имя несуществующего файла и использовать
+		 * его" не является атомарной операцией. В результате файл может быть
+		 * создан между проверкой на существование и вызовом конструктора
+		 * (следующая строка). В Linux насколько я знаю в именах файлов
+		 * недопустим символ "/", но, скажем, проверять наличие файла "x/y" не
+		 * имеет смысла, поскольку "x/y" будет интерпретирован как путь, который
+		 * теоретически может существовать.
 		 **/
 		new OnFileReader(new File("/"));
 	}
@@ -91,8 +100,9 @@ public class OnFileReaderTest extends OnFileReaderTestBase {
 		exception.expect(RuntimeException.class);
 		execute(x -> {
 			/**
-			 * TODO REVIEW(fdv): RuntimveException будет только тогда, когда в сообщении будет 'Some exception'? tdv: Нет, RuntimveException будет
-			 * всегда, независимо от текста.
+			 * TODO REVIEW(fdv): RuntimveException будет только тогда, когда в
+			 * сообщении будет 'Some exception'? tdv: Нет, RuntimveException
+			 * будет всегда, независимо от текста.
 			 **/
 
 			throw new IOException("Совершенно произвольный текст");
@@ -103,8 +113,10 @@ public class OnFileReaderTest extends OnFileReaderTestBase {
 	public static void testTmpFilesRemoval() {
 		Boolean exists = false;
 		/**
-		 * TODO REVIEW(fdv): Если закомментировать весь цикл (три следующих строки) то тест будет зеленым ) tdv: Это верно. А если закомментировать
-		 * все строки, то он тоже будет зелёным. Как и любой другой тест :)
+		 * TODO REVIEW(fdv): Если закомментировать весь цикл (три следующих
+		 * строки) то тест будет зеленым ) tdv: Это верно. А если
+		 * закомментировать все строки, то он тоже будет зелёным. Как и любой
+		 * другой тест :)
 		 **/
 		for (final String s : tmpFiles) {
 			final Path path = Paths.get(s);
