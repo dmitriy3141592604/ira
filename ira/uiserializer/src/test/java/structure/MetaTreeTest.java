19c6a2f761545ed25ade48a6c09f6dd00db034ca
Dmitry <User@WIN-T1J2J5B8A1T>
2017-01-30 21:44:11 +0200

    Добавлен MetaTree. Потом переименую.

---
+++ ira/uiserializer/src/test/java/structure/MetaTreeTest.java
@@ -0,0 +1,59 @@
+package structure;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+class Node extends MetaSupport {
+
+	private String name;
+
+	private final Map<String, Node> children = new TreeMap<String, Node>();
+
+	public Node() {
+	}
+
+	public Node select(String name) {
+		if (children.containsKey(name)) {
+			return children.get(name);
+		}
+		final Node node = new Node();
+		node.name = name;
+		children.put(name, node);
+		return node;
+	}
+
+	public String name() {
+		return name;
+	}
+
+}
+
+public class MetaTreeTest extends MetaTreeTestBase {
+
+	private Node mt;
+
+	@Before
+	public void setUpMetaTreeTestBase() {
+		mt = new Node();
+	}
+
+	@Test
+	public void test$selectSetNameForNextNode() {
+
+		final Node next = mt.select("edge");
+
+		assertEquals("edge", next.name());
+	}
+
+	@Test
+	public void test$selectIsRepeatable() {
+		assertSame(mt.select("edge"), mt.select("edge"));
+	}
+
+}
