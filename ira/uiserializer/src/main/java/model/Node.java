f373ae81bdb50e028e9b7bff6f53022985344641
Dmitry <User@WIN-T1J2J5B8A1T>
2017-02-10 23:28:21 +0200

    Отформатированы комментарии

--- ira/uiserializer/src/main/java/model/Node.java
+++ ira/uiserializer/src/main/java/model/Node.java
@@ -5,25 +5,17 @@ package model;
  */
 public class Node implements WithMarkersSupport {
 
-	/**
-	 * Имя узла модели.
-	 *
-	 * Должно быть не нулевым.
-	 **/
+	/** Имя узла модели. **/
 	private final String name;
 
 	private final MarkerSupport markers = new MarkerSupport();
 
-	/**
-	 * Создает узел с новым именем
-	 */
+	/** Создает узел с новым именем **/
 	public static Node newNode(String name) {
 		return new Node(name);
 	}
 
-	/**
-	 * Создает узел с новым именем
-	 */
+	/** Создает узел с новым именем **/
 	public Node(String name) {
 		this.name = name;
 	}
