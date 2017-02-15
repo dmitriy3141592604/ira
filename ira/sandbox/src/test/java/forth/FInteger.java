745aa65fab7ac3280d0341e12b8249c28adab88e
Frolov_D <Frolov_D@FROLOV_D.bpc.in>
2017-02-15 20:55:45 +0300

    SimpleFort added. DoubleDispetcherization example

---
+++ ira/sandbox/src/test/java/forth/FInteger.java
@@ -0,0 +1,36 @@
+package forth;
+
+public class FInteger extends FToken {
+
+	private final Integer impl;
+
+	public FInteger(Integer impl) {
+		this.impl = impl;
+	}
+
+	@Override
+	public void applay(FStack stack) {
+		stack.push(this);
+	}
+
+	@Override
+	public FToken summ(FToken top) {
+		return top.summ(this);
+	}
+
+	@Override
+	public FToken summ(FString top) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public FToken summ(FInteger top) {
+		return new FInteger(top.impl + this.impl);
+	}
+
+	@Override
+	public String toString() {
+		return impl.toString();
+	}
+
+}
---
+++ ira/sandbox/src/test/java/forth/FOperation.java
@@ -0,0 +1,33 @@
+package forth;
+
+import java.util.function.Consumer;
+
+public class FOperation extends FToken {
+
+	private final Consumer<FStack> impl;
+
+	public FOperation(Consumer<FStack> impl) {
+		this.impl = impl;
+	}
+
+	@Override
+	public void applay(FStack stack) {
+		impl.accept(stack);
+	}
+
+	@Override
+	public FToken summ(FToken top) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public FToken summ(FString top) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public FToken summ(FInteger top) {
+		throw new UnsupportedOperationException();
+	}
+
+}
