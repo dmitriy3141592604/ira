ddbf8ca534003143bd4887f4eb0c838bfc095a4a
Frolov_D <Frolov_D@FROLOV_D.bpc.in>
2017-02-22 19:02:06 +0300

    map tmp

--- ira/uiserializer/src/test/resources/model/generate.cmd
+++ ira/uiserializer/src/test/resources/model/generate.cmd
@@ -1,3 +1,5 @@
-echo off
-echo dsl
-dot -Tpng -odsl.png dsl.gv
+c:\wks\prg\Graphviz\bin\dot.exe -Tpng -oModelBasedDevelopment-dot.png ModelBasedDevelopment.gv
+rem c:\wks\prg\Graphviz\bin\twopi.exe -Tpng -oModelBasedDevelopment-twopi.png ModelBasedDevelopment.gv
+rem c:\wks\prg\Graphviz\bin\dot.exe -Tpng -Tcmapx -oModelBasedDevelopment-dot-mp.png ModelBasedDevelopment.gv > map.map
+rem c:\wks\prg\Graphviz\bin\circo.exe -Tpng -oModelBasedDevelopment-circo.png ModelBasedDevelopment.gv
+rem c:\wks\prg\Graphviz\bin\fdp.exe -Tpng -oModelBasedDevelopment-fdp.png ModelBasedDevelopment.gv
